bb9594a2653be5d869cd76427d2377b7c1044fad
RobertoJSJTV <robertojsj18@gmail.com>
2022-10-12 20:34:37 -0300

    adição e sl

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Styles from './styles/styles';
-import Home from './src/home'
+import Home from './src/home';
+import login from "./src/login";
 
 
 export default function App() {
   return (
     <SafeAreaView style={Styles.container}>
-      <Text>Hello World </Text>
-     
+      <login/>
+      
     </SafeAreaView>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Styles from "../styles/Login";
+
+export default () => {
+    <View><Text style={Styles.container}>Hello meu pau World</Text></View>
+
+  };
