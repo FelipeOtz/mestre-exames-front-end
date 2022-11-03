cf60dd8a36f72afc39b0674111c0dffb4664cb57
RobertoJSJTV <robertojsj18@gmail.com>
2022-11-02 21:38:03 -0300

    adição e sl

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput } from "react-native";
 import Styles from "../styles/Login";
 
 export default function Login() {
   return (
     <View style={Styles.container}>
-      <Image
-        source={require("../assets/logo-01-branco.png")}
-        style={{ width: 100, height: 100 }}
-        resizeMode="contain"
-      />
-
-      <View style={Styles.containerForm}>
-        <Text style={Styles.title}>Cuide de sua saúde fazendo exames</Text>
-        <Text style={Styles.text}>Faça o login para começar</Text>
-
-        <TouchableOpacity style={Styles.button}>
-          <Text style={Styles.buttonText}>Acessar</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={Styles.image}>
+          <Image
+            source={require('../assets/logo-01-azul.png')}
+            style={{width: 100, height: 100}}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={Styles.containerForm}>
+          <Text style={Styles.title}>Login</Text>
+          <Text style={Styles.text}>Email</Text>
+          <TextInput
+          style={Styles.input}
+          />
+        
+          <TouchableOpacity style={Styles.button}>
+            <Text style={Styles.buttonText}>entrar</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
--- styles/Login.js
+++ styles/Login.js
@@ -1,17 +1,64 @@
 import { StyleSheet } from "react-native";
+import { Dimensions, Platform, StatusBar } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    width: "100%",
-    height: "100%",
-    flex: 1,
-    backgroundColor: "#3779f4",
-    alignItems: "center",
-    justifyContent: "center",
-    color: "orange",
-  },
-
-  containerForm: {
-    backgroundColor: "#FFFFFF",
-  },
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+     width: '100%',
+     height: '100%',
+     flex: 1,
+     backgroundColor: "#FFF",
+     alignItems: "center",
+     justifyContent: "center",
+     color: 'orange',
+     paddingStart: 10
+   },
+   image:{
+     alignSelf: "flex-start"
+   },
+   containerLogo:{
+     flex:2,
+     justifyContent: 'center',
+     alignItems: 'center'
+   },
+   containerForm:{
+     flex:1,
+     backgroundColor: '#FFF',
+     borderTopLeftRadius: 25,
+     borderTopRightRadius: 25,
+     paddingStart: '5%',
+     paddingEnd: '5%'
+   },
+   title:{
+     fontSize: 24,
+     fontWeight: 'bold',
+     color: '#3785F4',
+     marginTop: 28,
+     marginBottom: 12,
+   },
+   text:{
+     color: '#a1a1a1'
+   },
+   button:{
+     position: 'absolute',
+     backgroundColor: '#3785F4',
+     borderRadius: 50,
+     paddingVertical: 8,
+     width: '60%',
+     alignSelf: 'center',
+     bottom: '15%',
+     alignItems: 'center',
+     justifyContent: 'center'
+   },
+   buttonText:{
+     fontSize: 18,
+     color: '#FFF',
+     fontWeight: 'bold'
+   },
+   input: {
+     height: 40,
+     margin: 12,
+     borderWidth: 1,
+     padding: 10,
+   },
 });
