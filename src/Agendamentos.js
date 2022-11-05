15b53f748753876d85900b5a609be6c835b70847
FelipeOtz <felipeht25@gmail.com>
2022-11-05 17:52:27 -0300

    Arrumando StatusBar para ios

--- src/Agendamentos.js
+++ src/Agendamentos.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Text, View, TouchableHighlight, Image, Alert } from "react-native";
-import CardsAgendamento from './components/CardsAgendamento';
+import CardsAgendamento from "./components/CardsAgendamento";
 import Styles from "../styles/Agendamentos";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-
-export default function(){
-    return(
-        <View style={Styles.container}>
-            <View style={Styles.header}>
-                <TouchableOpacity
-                    onPress={()=> Alert.alert('Clicou', 'Clickou em Voltar')}
-                    underlayColor='transparent'>
-                    <Image source={require('../assets/icons/return-arrow-1.png')} style={Styles.icon}/>
-                </TouchableOpacity>
-                <Text style={Styles.title}>
-                    Meus Agendamentos
-                </Text>
-                <View></View>
-            </View>
-            <View style={{width: '100%', paddingHorizontal: 10, height: '85%'}}>
-                <CardsAgendamento/>
-            </View>
-        </View>
-    )
+export default function () {
+  return (
+    <View style={Styles.container}>
+      <View style={Styles.header}>
+        <TouchableOpacity
+          onPress={() => Alert.alert("Clicou", "Clickou em Voltar")}
+          underlayColor="transparent"
+        >
+          <Image
+            source={require("../assets/icons/return-arrow-1.png")}
+            style={Styles.icon}
+          />
+        </TouchableOpacity>
+        <Text style={Styles.title}>Meus Agendamentos</Text>
+        <View></View>
+      </View>
+      <View style={{ width: "100%", paddingHorizontal: 10, height: "85%" }}>
+        <CardsAgendamento />
+      </View>
+    </View>
+  );
 }
--- styles/Login.js
+++ styles/Login.js
@@ -1,115 +1,114 @@
 import { StyleSheet } from "react-native";
-import { Dimensions, Platform, StatusBar } from 'react-native';
+import { Dimensions, Platform, StatusBar } from "react-native";
 
 export default StyleSheet.create({
   container: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-     width: '100%',
-     height: '100%',
-     flex: 1,
-     backgroundColor: "#FFF",
-     alignItems: "center",
-     justifyContent: "space-between",
-     color: 'orange',
-     paddingStart: 10
-   },
-   image:{
-     alignSelf: "flex-start"
-   },
-   containerLogo:{
-     flex:2,
-     justifyContent: 'center',
-     alignItems: 'center'
-   },
-   containerForm:{
-     flex:1,
-     backgroundColor: '#FFF',
-     borderTopLeftRadius: 25,
-     borderTopRightRadius: 25,
-     paddingStart: '5%',
-     paddingEnd: '5%',
-     width: '100%',
-
-   },
-   title:{
-     fontSize: 24,
-     alignItems: "center",
-     justifyContent: "center",
-     fontWeight: 'bold',
-     color: '#3785F4',
-     marginTop: 15,
-     marginBottom: 12,
-   },
-   text:{
-     color: '#a1a1a1',
-     paddingStart: 10,
-     color: '#3785F4',
-   },
-   button:{
-     backgroundColor: '#3785F4',
-     paddingVertical: 8,
-     width: '95%',
-     alignSelf: 'center',
-     alignItems: 'center',
-     justifyContent: 'center'
-   },
-   buttonText:{
-     fontSize: 18,
-     color: '#FFF',
-     fontWeight: 'bold',
-     width: '100%',
-     textAlign: 'center'
-   },
-   input: {
-     height: 40,
-     margin: 12,
-     borderWidth: 1,
-     padding: 10,
-     width: '95%',
-     borderRadius: 5,
-     borderColor: '#3785F4',
-     alignSelf: 'center'
-   },
-   forgot:{
-    color: '#F9C259',
-    paddingStart: 10
-   },
-   socialButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: '5%',
-   },
-   enterUsing: {
-    marginTop: '25%',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 50,
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "space-between",
+    color: "orange",
+    paddingStart: 10,
+  },
+  image: {
+    alignSelf: "flex-start",
+  },
+  containerLogo: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  containerForm: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: "5%",
+    paddingEnd: "5%",
+    width: "100%",
+  },
+  title: {
+    fontSize: 24,
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+    color: "#3785F4",
+    marginTop: 15,
+    marginBottom: 12,
+  },
+  text: {
+    color: "#a1a1a1",
+    paddingStart: 10,
+    color: "#3785F4",
+  },
+  button: {
+    backgroundColor: "#3785F4",
+    paddingVertical: 8,
+    width: "95%",
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#FFF",
+    fontWeight: "bold",
+    width: "100%",
+    textAlign: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: "95%",
+    borderRadius: 5,
+    borderColor: "#3785F4",
+    alignSelf: "center",
+  },
+  forgot: {
+    color: "#F9C259",
+    paddingStart: 10,
+  },
+  socialButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: "5%",
+  },
+  enterUsing: {
+    marginTop: "25%",
     color: "#939CAA",
-    alignSelf: 'center',
+    alignSelf: "center",
     fontSize: 16,
-    fontWeight: 'bold'
-   },
-   mediaIcon:{
+    fontWeight: "bold",
+  },
+  mediaIcon: {
     width: 35,
     height: 35,
-   },
-   link:{
+  },
+  link: {
     backgroundColor: "#1DA1F2",
     width: 135,
     height: 45,
     marginVertical: 25,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly'
-   },
-   linkTxt: {
-    fontWeight: 'bold',
-    color: '#FFF',
-    fontSize: 17
-   },
-   links: {
-    width: '80%',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  linkTxt: {
+    fontWeight: "bold",
+    color: "#FFF",
+    fontSize: 17,
+  },
+  links: {
+    width: "80%",
+    alignItems: "center",
     flex: 1,
     flexDirection: "row",
-   },
+  },
   //  buttonTwitter:{
   //   backgroundColor: '#1DA1F2',
   //   paddingVertical: 8,
@@ -127,19 +126,19 @@ export default StyleSheet.create({
   //   flexDirection: "row",
   //   flex: 1
   //  },
-   buttonText:{
+  buttonText: {
     fontSize: 12,
-    color: '#FFF',
-    fontWeight: 'bold'
+    color: "#FFF",
+    fontWeight: "bold",
   },
-  cadastreSe :{
-    alignSelf: 'center',
+  cadastreSe: {
+    alignSelf: "center",
 
-    flexDirection: 'row'
+    flexDirection: "row",
   },
-  dontHaveAccount :{
+  dontHaveAccount: {
     color: "#939CAA",
     fontSize: 16,
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
 });
